f05feee72cb4825757ff48031465eec814672200
AGfinx <abhijeetgiri1205@gmail.com>
2025-06-26 19:41:45 +0530

    new2

---
+++ script.js
@@ -0,0 +1,22 @@
+function greet() {
+    alert("Hello World !!!")
+}
+
+const number = [1, 2, 3, 4, 5]
+
+const smallest = Math.min(...number)
+const largest = Math.max(...number)
+
+console.log("Smallest number", smallest)
+console.log("Largest number", largest)
+
+const bike ={
+    model: "Yamaha MT-15",
+    year: "2021",
+    color: "black",
+    price: 150000
+}
+
+// function displayInfo(){
+//     console.log('model :', ${this.model}, 'Year :' ${this.year})
+// }
